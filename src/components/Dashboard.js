d29489e996c5f780f273908457b7854220fcac39
Kelsi Olstad <kelsiolstad@gmail.com>
2022-04-01 20:16:00 -0600

    create Loading component and import to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import Loading from "./Loading.js";
+
 import classnames from "classnames";
 
 class Dashboard extends Component {
+  state = { loading: false };
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
